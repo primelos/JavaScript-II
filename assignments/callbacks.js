455075b2e0bf7dc0ae9ebc6aad808a585ed8a2b3
Ale <alearanibar@gmail.com>
2019-08-20 09:08:58 -0700

    first update

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,7 +1,11 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your 
+//work. You have been provided an example of a problem and a solution to see how 
+//this works with our items array.  Study both the problem and the solution to 
+//figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+ 
+console.log(items[items.length -1])
 /* 
 
   // GIVEN THIS PROBLEM:
@@ -12,8 +16,8 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // SOLUTION:
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
+  //////////function firstItem(arr, cb) {
+    //////////return cb(arr[0]);
   }
 
   // NOTES ON THE SOLUTION:
@@ -41,19 +45,27 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+getLength( items, l => console.log(`my length is ${l}`))
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length -1])
 }
+last(items, lItem => console.log(`last in the array is ${lItem}`))
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+sumNums(2,6, sum => console.log(`${sum}`))
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+multiplyNums(3,9, product => console.log(`${product}`))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -66,4 +78,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
 }
